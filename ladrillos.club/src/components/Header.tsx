00ce2bbbc4afe777ed079e1d1de3e0b00e2da558
202745EsquedDaniel <eduardani84@gmail.com>
2025-01-08 22:08:12 -0700

    manejo de templates astro

--- ladrillos.club/src/components/Header.tsx
+++ ladrillos.club/src/components/Header.tsx
@@ -6,8 +6,8 @@ interface HeaderProps {
 
 const Header: React.FC<HeaderProps> = ({ title }) => {
     return (
-        <header className='container mx-auto p-4'>
-            <h1 className='text-4xl text-sky-800'>{title}</h1>
+        <header className='container max-w-screen-2xl mx-auto p-4'>
+        <h1 className='text-4xl text-sky-800 font-bold'>{title}</h1>
         </header>
     )
 };
